25a3203b1795bbb2e4abe4613c46fb6bb92ae9d9
juniper wilde <juniper.r.wilde@gmail.com>
2019-05-08 09:51:35 -0700

    create model layer for db

--- server/src/model/jobs.js
+++ server/src/model/jobs.js
@@ -1,14 +1,79 @@
 import database from './database';
 
-function createJob(revision, status) {
+const createJob = (test_tag, build_tag) => {
   return database.one(
-    `INSERT INTO jobs(revision, status)
+    `INSERT INTO jobs(test_tag, build_tag)
     VALUES ($1, $2)
     RETURNING *`,
-    [revision, status]
+    [test_tag, build_tag]
   );
-}
+};
+
+const getAllJobs = () => {
+  return database.any(
+    `SELECT *
+    FROM jobs`
+  );
+};
+
+const getLatestJobs = () => {
+  return database.any(
+    `SELECT *
+    FROM jobs
+    ORDER BY created_at ASC
+    LIMIT 10`
+  );
+};
+
+const getJobById = (job_id) => {
+  return database.one(
+    `SELECT *
+    FROM jobs
+    WHERE id = $1`,
+    [job_id]
+  );
+};
+
+const getNewJobs = () => {
+  return database.any(
+    `SELECT *
+    FROM jobs
+    WHERE job_status = 'new'`
+  );
+};
+
+const getPendingJobs = () => {
+  return database.any(
+    `SELECT *
+    FROM jobs
+    WHERE job_status = 'pending'`
+  );
+};
+
+const getCompletedJobs = () => {
+  return database.any(
+    `SELECT *
+      FROM jobs
+      WHERE job_status = 'completed'`
+  );
+};
+
+const updateJob = (job_id, status, build_flags) => {
+  return database.one(
+    `UPDATE jobs
+    SET status = $1, build_flags = $2
+    WHERE id = $3`,
+    [job_id, status, build_flags]
+  );
+};
 
 export default {
   createJob,
-}
+  getAllJobs,
+  getLatestJobs,
+  getJobById,
+  getNewJobs,
+  getPendingJobs,
+  getCompletedJobs,
+  updateJob,
+};
--- server/src/model/logs.js
+++ server/src/model/logs.js
@@ -1,1 +1,34 @@
 import database from './database';
+
+const createLog = (job_id) => {
+  return database.one(
+    `INSERT INTO logs (job_id)
+    VALUES ($1)
+    RETURNING *`,
+    [job_id]
+  );
+};
+
+const getLogById = (log_id) => {
+  return database.one(
+    `SELECT *
+    FROM logs
+    WHERE id = $1`,
+    [log_id]
+  );
+};
+
+const getLogByJobId = (job_id) => {
+  return database.any(
+    `SELECT *
+    FROM logs
+    WHERE job_id = $1`,
+    [job_id]
+  );
+};
+
+export default {
+  createLog,
+  getLogById,
+  getLogByJobId,
+};
